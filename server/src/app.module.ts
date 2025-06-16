f4e1b0fda8659c16f6c351b33111179f053ad9a3
Hick Andrade <henrique_andrade2013@hotmail.com>
2025-06-16 20:35:08 -0300

    chore: monorepo setup with NestJS (server) and Vite + React (client)

---
+++ server/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: false,
+        entities: [join(__dirname, '**/*.entity.{ts,js}')],
+        migrations: [join(__dirname, 'migrations/*.{ts,js}')]
+      })
+    })
+  ],
+   
+})
+export class AppModule {}
